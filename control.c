cd2bcb6a4ce0af66ddf6a3f05015603b88a686c0
Tao Lin <lin.tao.5711y@gmail.com>
2016-12-08 15:31:47 -0500

    semaphore creation

--- control.c
+++ control.c
@@ -7,25 +7,28 @@
 
 union semun{
   int val; //val for set val
-  struct semid_ds; //Buffer for IPC_STAT IPC_SET
+  struct semid_ds *buf; //Buffer for IPC_STAT IPC_SET
   unsigned short *array; //Array for GETALL , SETALL
   struct seminfo *__buf; //Buffer for IPC_INFO
 };
 
 int main(int argc , char ** argv){
-  
+  int semid;
+  int key = ftok("control.c", 512);
+  int semC;
   int size;
   
   if(argc == 0){
     printf("add one to commandline: -c, -r, -v\n");
     return -1;
   }
-  if(strcmp(argv[1], "-c") == 0){
-    int key = ftok("control.c" , 512);
-    int semid = semget(key, 1 , IPC_EXCL | IPC_CREAT | 0644);
-    int sm = shmget(key, 1024 , IPC_CREAT | IPC_EXCL | 0644);
-    
-    open("writefile" O_TRUNC | O_CREAT | 0644);
+  
+  if(strncmp(argv[1], "-c", strlen(argv[1])) == 0){
+    semid = semget(key, 1, IPC_CREAT | IPC_EXCL | 0644);
+    printf("semaphore created: %d\n", semid);
+    union semun su;
+    su.val = 1;
+    semC = semctl(semid, 0, SETVAL, su);
     return 0;
   }
   
